9938b171fd77e606d1a5a67b36813b1d0073039e
Jason Hendry <jason@rain.com.au>
2026-01-19 20:30:20 +1100

    feat: add Access-Control-Allow-Origin header to API Gateway response.

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"context"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+func orginList(origins []string) string {
+	return strings.Join(origins, ", ")
+}
+
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	origins := []string{
+		"https://committee.eurekacycling.org.au",
+		"https://committee2.eurekacycling.org.au",
+	}
+
 	return events.APIGatewayProxyResponse{
 		Body:       "Hello, Eureka Cycling Club!",
 		StatusCode: 200,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": orginList(origins),
+		},
 	}, nil
 }
 
