c036a94408a1f6b0672ccfbf1ca5a9684d3b5b49
Jason <jason@portable.com.au>
2026-01-31 09:54:57 +1100

    test(cypress): validate ledger month titles

--- frontend/cypress/e2e/app.cy.ts
+++ frontend/cypress/e2e/app.cy.ts
@@ -7,5 +7,14 @@ describe('app', () => {
     cy.get('input[name="username"]').type(username);
     cy.get('input[name="password"]').type(password);
     cy.get('button[type="submit"]').click();
+    cy.contains('a', 'Ledger').click();
+    cy.get('.month-block').each(($block) => {
+      cy.wrap($block)
+        .find('.month-title > h3')
+        .invoke('text')
+        .then((text) => {
+          expect(text.trim()).to.not.equal('');
+        });
+    });
   });
 });
